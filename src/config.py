ca97738b61c2c4e3c1110f02ba943dc94ef2d123
Sarowar Jahan Saurav <64561923+SarowarSaurav@users.noreply.github.com>
2023-06-06 02:23:53 +0600

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
-username = "+8801723068659"
-password = "emotional66"
+username = ""
+password = ""
 number_posts_max = 10 #Number of Posts to Scrape
 number_comments_max = 10 #Number of Comments to Extract by each Post
 output_file_name = 'fb_scraper_output.csv'
